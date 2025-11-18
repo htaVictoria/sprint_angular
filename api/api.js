e3a7b4e3a8874e40007405b59c9489f080a218de
htaVictoria <agatharocha0120@gmail.com>
2025-11-18 16:16:20 -0300

    .

--- api/api.js
+++ api/api.js
@@ -8,7 +8,7 @@ app.use(cors());
 // app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname)));
+app.use('/img', express.static(path.join(__dirname, 'img')));
 
 app.post("/login", async (req, res) => {
     try {
